e3813f11b0c1b45fb621f9f28991d1dd3d71da08
Brendan Tang <b@brendantang.net>
2020-07-08 17:01:48 -0400

    delete extraneous file

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-app = express()
-const port = 9090
-
-app.use(express.static('site'))
-
-app.listen(port, () => console.log(`listening on http://localhost:${port}`))
