5fe0157e1a26cd4aed6b60beb18cd80e41f6877b
Brendan Tang <b@brendantang.net>
2020-07-28 11:50:59 -0400

    polish scrolling style

---
+++ src/components/notes_index.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+const NotesIndex = ({ notes }) => {
+  const posts = notes 
+
+  return (
+    <div>
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+          <article key={node.fields.slug}>
+            <header>
+              <h3 >
+                <Link to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+            </header>
+            <section>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </section>
+          </article>
+        )
+      })}
+    </div>
+  )
+}
+
+export default NotesIndex 
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,14 @@ import { Link, graphql } from "gatsby"
 
 import Doodle from "../components/doodle"
 import SEO from "../components/seo"
+import NotesIndex from "../components/notes_index.js"
 
 import style from "../styles/home.module.css"
 
 const Home = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
+
+  const notes = data.allMarkdownRemark.edges
 
   return (
     <main className={style.wrapper}>
@@ -16,7 +19,10 @@ const Home = ({ data, location }) => {
         <Doodle name="front"/>
       </section>
       <section className={style.block}>
-        <Doodle name="notes"/>
+        <div className={style.blockSection}>
+          <Doodle name="notes"/>
+          <NotesIndex notes={notes} />
+        </div>
       </section>
 
     </main>
@@ -30,6 +36,21 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
       }
     }
   }
